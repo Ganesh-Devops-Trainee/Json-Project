f45e11b819f733b39c1596612a80e9273d53bac3
Ganesh-Devops-Trainee <ganeshrao15061998@gmail.com>
2024-11-21 02:08:18 +0530

    Update downloadJson.ts

--- dynamic-form-generator/src/utils/downloadJson.ts
+++ dynamic-form-generator/src/utils/downloadJson.ts
@@ -2,6 +2,6 @@ export default function downloadJson(data: any, filename: string) {
   const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
   const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
-  link.download = ${filename}.json;
+  link.download = ${filename}."json";
   link.click();
 }
