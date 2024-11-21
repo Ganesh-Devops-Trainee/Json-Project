bd9a5c2eb2ac0dbc22ec43ac4881c2ef37ba3718
Ganesh-Devops-Trainee <ganeshrao15061998@gmail.com>
2024-11-21 07:15:08 +0530

    update

--- dynamic-form-generator/tailwind.config.js
+++ dynamic-form-generator/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
   ],
   darkMode: 'class', // Enable dark mode with the "class" strategy
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      lightText: '#2D3748', // Custom text color for light mode
+      darkText: '#E2E8F0',  // Custom text color for dark mode
+    },
   },
+},
   plugins: [],
 };
+// module.exports = {
+//   darkMode: 'class', // Enable dark mode via class
+//   theme: {
+//     extend: {
+//       colors: {
+//         lightText: '#2D3748', // Custom text color for light mode
+//         darkText: '#E2E8F0',  // Custom text color for dark mode
+//       },
+//     },
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
